01a321247edbb742b917ff91399666fe903726d9
john Meng <john.meng014@gmail.com>
2019-11-13 12:58:40 -0500

    added the javascript for the survey html and the other just files for routing can't get it to show on the termanl

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendList = [
+    {
+        name: 'John',
+        profilePic: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiKware2-flAhUBd98KHUCLAJcQjRx6BAgBEAQ&url=https%3A%2F%2Fonepiece.fandom.com%2Fwiki%2FMonkey_D._Luffy&psig=AOvVaw0pzebWlCfr9mVdxlF-RsET&ust=1573752680424300',
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: 'Sam',
+        profilePic: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiorbzn2-flAhWsV98KHSMSA8YQjRx6BAgBEAQ&url=https%3A%2F%2Fcomicvine.gamespot.com%2Froronoa-zoro%2F4005-47925%2F&psig=AOvVaw3-GoWF0XzMGAyFjMjm1N8t&ust=1573752700819400',
+        scores: [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ]
+    }
+]
+
+module.exports = friendList;
